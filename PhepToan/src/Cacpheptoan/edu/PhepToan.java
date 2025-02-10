cdcf29ec3eaac04f2af64326682f67e0ddc9f6ac
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-11 06:53:04 +0700

    Bài tập các phép toán
    phép toán nhập a và b và in ra c = a + b

---
+++ PhepToan/src/Cacpheptoan/edu/PhepToan.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package Cacpheptoan.edu;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class PhepToan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Tạo một đối tượng Scanner
+		Scanner scanner = new Scanner(System.in);
+		
+		// Nhập giá trị a
+		System.out.print("Xin mời nhập số a: ");
+		int a = scanner.nextInt();
+		
+		// Nhập giá trị b
+		System.out.print("Xin mời nhập số b: ");
+		int b = scanner.nextInt();
+		
+		// Tính tổng a và b
+		int c = a + b;
+		
+		// In ra ket qua
+		System.out.print("Tổng của a và b là: "+c);
+	}
+
+}
