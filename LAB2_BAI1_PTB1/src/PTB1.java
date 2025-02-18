385eda5a07dd3ac78132d2f61ca7c5fb3cb9913b
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-18 07:45:52 +0700

    Update PTB1.java

--- LAB2_BAI1_PTB1/src/PTB1.java
+++ LAB2_BAI1_PTB1/src/PTB1.java
@@ -6,28 +6,39 @@ public class PTB1 {
 		// TODO Auto-generated method stub
 		// Khai báo Scanner để nhập dữ liệu từ người dùng
 		Scanner scanner = new Scanner(System.in);
+		String tieptuc;
 		
-		// Phương trình bậc nhất có dạng ax + b = 0, nhận 2 tham số đầu vào a, b
+		do {
+			// Phương trình bậc nhất có dạng ax + b = 0, nhận 2 tham số đầu vào a, b
 		
-		// Yêu cầu người dùng nhập giá trị cho 2 biến a, b
-		System.out.print("Xin mời nhập a: ");
-		double a = scanner.nextDouble();
+			// Yêu cầu người dùng nhập giá trị cho 2 biến a, b
+			System.out.print("Xin mời nhập a: ");
+			double a = scanner.nextDouble();
 		
-		System.out.print("Xin mời nhập b: ");
-		double b = scanner.nextDouble();
+			System.out.print("Xin mời nhập b: ");
+			double b = scanner.nextDouble();
 		
-		// Giải và biện luận phương trình bậc nhất
-		if (a == 0) {
-			if (b == 0) {
+			// Giải và biện luận phương trình bậc nhất
+			if (a == 0) {
+				if (b == 0) {
 				System.out.print("Phương trình có vô số nghiệm");
+				}
+				else {
+					System.out.print("Phương trình vô nghiệm");
+				}
 			}
 			else {
-				System.out.print("Phương trình vô nghiệm");
+				System.out.printf("Nghiệm của phương trình x = -b/a = %.2f\n", (-b/a));
 			}
-		}
-		else {
-			System.out.printf("Nghiệm của phương trình x = -b/a = %.2f", (-b/a));
-		}
+		
+			// Hỏi người dùng có muốn tạo mới phép tính hay không
+			System.out.print("Bạn có muốn tiếp tục không? nếu có thì hãy nhập y để tiếp tục nhé, nhập phím khác sẽ thoát: ");
+			tieptuc = scanner.next();
+		} while (tieptuc.equalsIgnoreCase("y")); // nhấn Y lớn vẫn chấp nhận
+		
+		System.out.print("Chương trình đã kết thúc, tạm biệt bạn!");
+		
+		// Đóng Scanner
+		scanner.close();
 	}
-
 }
