371319002b48dccdbb6312770af847802e33b7e0
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-18 10:00:38 +0700

    New update

--- LAB2_BAI2_PTB2/src/PTB2.java
+++ LAB2_BAI2_PTB2/src/PTB2.java
@@ -6,14 +6,35 @@ class PTB1{
 	//	Phương trình bậc nhất: ax + b = 0
 	public void PTB1(double a, double b) {
 		if (a == 0) {
-			if (b == 0)
+			if (b == 0) {
+				System.out.print("Phương trình có VSN");
+				
+			}
+			else {
+				System.out.print("Phương trình VN");
+			}
+		}
+		else {
+			System.out.printf("Nghiệm của phương trình x = -b/a = %.2f/n", (-b/a));
 		}
 	}
 	
 }
 
-public class PTB2 {
-
+public class PTB2 extends PTB1 {
+	// Phương trình bậc nhị: ax2 + bx + c
+	public void PTB2(double a, double b, double c) {
+		if (a == 0) {
+			// Phương trình quay về phương trình bậc nhất
+			PTB1(b, c);
+		} else {
+			double del = Math.pow(b, 2) - 4*a*c;
+			
+			if  (del < 0) { 
+				System.out.print("Phương trình VN");
+			}
+		}
+	}
 	
 
 }
