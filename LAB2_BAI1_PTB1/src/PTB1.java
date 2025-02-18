16c92a4aaa75d574a821f63b9c0c49df333fffda
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-18 07:25:09 +0700

    Update PTB1.java

--- LAB2_BAI1_PTB1/src/PTB1.java
+++ LAB2_BAI1_PTB1/src/PTB1.java
@@ -8,17 +8,25 @@ public class PTB1 {
 		Scanner scanner = new Scanner(System.in);
 		
 		// Phương trình bậc nhất có dạng ax + b = 0, nhận 2 tham số đầu vào a, b
-		double a, b;
+		
+		// Yêu cầu người dùng nhập giá trị cho 2 biến a, b
+		System.out.print("Xin mời nhập a: ");
+		double a = scanner.nextDouble();
+		
+		System.out.print("Xin mời nhập b: ")
+		double b = scanner.nextDouble()
+		
+		// Giải và biện luận phương trình bậc nhất
 		if (a == 0) {
 			if (b == 0) {
 				System.out.print("> Phương trình có vô số nghiệm");
 			}
 			else {
-				System.out.print("Phương trình vô nghiệm");
+				System.out.print("> Phương trình vô nghiệm");
 			}
 		}
 		else {
-			System.out.print("Nghiệm của phương trình x = -b/a = " + (-b/a));
+			System.out.printf("> Nghiệm của phương trình x = -b/a = %.2f" + (-b/a));
 		}
 	}
 
