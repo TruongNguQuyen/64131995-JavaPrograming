a3c9830a601620da62f510aabbff6ecfea4ee94d
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-18 08:17:49 +0700

    Update PTB2.java

--- LAB2_BAI2_PTB2/src/PTB2.java
+++ LAB2_BAI2_PTB2/src/PTB2.java
@@ -1,18 +1,19 @@
-/**
- * 
- */
 
-/**
- * 
- */
+import java.util.Scanner;
+
+	// Lớp giải phương trình bậc 1
+class PTB1{
+	//	Phương trình bậc nhất: ax + b = 0
+	public void PTB1(double a, double b) {
+		if (a == 0) {
+			if (b == 0)
+		}
+	}
+	
+}
+
 public class PTB2 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
