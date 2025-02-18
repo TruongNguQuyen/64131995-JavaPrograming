a7439c6ff762c3d6b1c0555f71040c575bd1ea3f
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-18 07:19:19 +0700

    Update PTB1.java

--- LAB2_BAI1_PTB1/src/PTB1.java
+++ LAB2_BAI1_PTB1/src/PTB1.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class PTB1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Khai báo Scanner để nhập dữ liệu từ người dùng
+		Scanner scanner = new Scanner(System.in);
+		
+		// Phương trình bậc nhất có dạng ax + b = 0, nhận 2 tham số đầu vào a, b
+		double a, b;
+		if (a == 0) {
+			if (b == 0) {
+				System.out.print("> Phương trình có vô số nghiệm");
+			}
+			else {
+				System.out.print("Phương trình vô nghiệm");
+			}
+		}
+		else {
+			System.out.print("Nghiệm của phương trình x = -b/a = " + (-b/a));
+		}
 	}
 
 }
