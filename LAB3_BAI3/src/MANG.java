82a6ad6e7a674fffb253f6fcca794712d9adbf16
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-19 08:49:59 +0700

    Update MANG.java

--- LAB3_BAI3/src/MANG.java
+++ LAB3_BAI3/src/MANG.java
@@ -23,7 +23,31 @@ public class MANG {
 		Arrays.sort(a);
 		
 		// Xuất mảng vừa nhập ra màn hình
-		System.out.print("Mảng sau khi sắp xếp: " + Arrays.toString(a));
+		System.out.println("Mảng sau khi sắp xếp: " + Arrays.toString(a));
+		
+		// Xuất ra phần tử bé nhất
+		System.out.println("Phần tử bé nhất là: " + a[0]);
+		
+		// Tính trung bình cộng các phần tử chia hết cho 3
+		int sum = 0;
+		int dem = 0;
+		for (int x : a) {
+			if (x % 3 == 0) {
+				sum += x;
+				dem++;
+			}
+		}
+		
+		// Xuất trung bình cộng các phần tử chia hết cho 3
+		if (dem > 0) {
+			double TBCong = (double) sum/dem;
+			System.out.printf("Trung bình cộng các phần tử chia hết cho 3: %.2f", TBCong);
+		} else {
+			System.out.print("Không có phần tử nào trong mảng chia hết cho 3");
+		}
+		
+		// Đóng scanner
+		scanner.close();
 	}
 
 }
